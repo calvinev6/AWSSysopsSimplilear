593f5396a13fca4760569358a52734c061956db8
calvine6 <calvinev6@yahoo.com>
2019-10-10 01:17:43 +0000

    2nd commmit for demo.java

--- demo.java
+++ demo.java
@@ -1,2 +1,5 @@
+I'll like to add some more information to the previous code is that okay?
+
+Let me know
 How many times will you listen to me talk to you about being observant of little things
 Dont you know this too
