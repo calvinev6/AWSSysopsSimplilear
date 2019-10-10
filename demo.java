3f6a372db4a6af8e50475e77765f31ae10186153
calvine6 <calvinev6@yahoo.com>
2019-10-10 01:42:10 +0000

    commit all files

--- demo.java
+++ demo.java
@@ -3,3 +3,5 @@ I'll like to add some more information to the previous code is that okay?
 Let me know
 How many times will you listen to me talk to you about being observant of little things
 Dont you know this too
+As you may know I wasn't impressed iwht you 
+I can look back too you know!!
